cf355ca6ed2fe2feb2d85a5a6c7ff01be57215a3
Yash Gupta <yash.gupta@mail.mcgill.ca>
2016-02-01 07:18:53 -0500

    PrettyPrinter works better now.

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,7 @@ module Main (main) where
 
 import Scanner
 import Parser
-import PrettyPrint
+import PrettyPrinter
 import qualified Control.Exception as E
 
 filterfn (S_Error _) = False
@@ -20,7 +20,9 @@ final s = unlines $ prettyPrint (parse (scan s))
 
 main = do
  s <- getContents
- putStr $ show ((final s) == (final (final s)))
+ let t = final s
+ putStr t
+ putStr $ show (t == (final t))
  --E.catch (putStrLn ("VALID: " ++(show (final s))))
   --(\(E.ErrorCall e) -> putStrLn ("INVALID: Parse Error at "++ e))
 
